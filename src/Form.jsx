0b9bb1aba52e4dfef41e6ea7fe312ab00d21c7a5
vibhavdubey <vibhavkumardubey19@gmail.com>
2025-01-06 14:42:03 +0530

    update

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState } from "react";
+import { useState } from "react";
 function Form() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
