9c0aadc518669b520bee1be6686bb8a0bbc9fc56
vibhavdubey <vibhavkumardubey19@gmail.com>
2025-01-06 14:59:25 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <section>
       <Routes>
-        <Route path="/" element={<Form />} />
+        <Route path="/CRM-Form" element={<Form />} />
       </Routes>
     </section>
   );
